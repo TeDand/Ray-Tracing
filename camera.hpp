ad2cb7b028b501c3cbfa4719bf8ac82b6c9ad759
TeDand <tejas-d@hotmail.com>
2020-08-20 09:54:15 +0100

    added different camera angles and fov

--- camera.hpp
+++ camera.hpp
@@ -5,20 +5,36 @@
 
 class camera {
     public:
-        camera() {
-            auto aspect_ratio = 16.0 / 9.0;
-            auto viewport_height = 2.0;
+        camera(
+            point3 lookfrom,
+            point3 lookat,
+            vec3   vup,
+            double vfov, // vertical field-of-view in degrees
+            double aspect_ratio
+            ) 
+        {
+            // field of view calculation to get the height and width of the viewport
+            auto theta = degrees_to_radians(vfov);
+            auto h = tan(theta/2);
+            auto viewport_height = 2.0 * h;
             auto viewport_width = aspect_ratio * viewport_height;
-            auto focal_length = 1.0;
 
-            origin = point3(0, 0, 0);
-            horizontal = vec3(viewport_width, 0.0, 0.0);
-            vertical = vec3(0.0, viewport_height, 0.0);
-            lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+            // creates the uv plane with the w vector being the normal to the plan in the direction we are looking
+            auto w = unit_vector(lookfrom - lookat);
+            // note vup is in the same plane as uv
+            auto u = unit_vector(cross(vup, w));
+            auto v = cross(w, u);
+
+            // orients the camera to view the frame from different angles
+            origin = lookfrom;
+            horizontal = viewport_width * u;
+            vertical = viewport_height * v;
+            lower_left_corner = origin - horizontal/2 - vertical/2 - w;
         }
 
-        ray get_ray(double u, double v) const {
-            return ray(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+        // the rays are still coming from the origin
+        ray get_ray(double s, double t) const {
+            return ray(origin, lower_left_corner + s*horizontal + t*vertical - origin);
         }
 
     private:
